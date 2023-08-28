8238071b512fe40f8809abb3b55a198f8b121278
zcxb <chen13157379235@outlook.com>
2023-08-28 23:01:56 +0800

    update collector

--- otona_book_collector/src/jobs/avmoo/index.js
+++ otona_book_collector/src/jobs/avmoo/index.js
@@ -1,5 +1,55 @@
 const { createPage } = require('../../utils/browser');
 const dayjs = require('dayjs');
+const axios = require('axios');
+const https = require('https');
+// const { uploadBuffer } = require('../../utils/api');
+const FormData = require('form-data');
+var parseUrl = require('url').parse;
+const agent = new https.Agent({ rejectUnauthorized: false });
+
+const uploadBuffer = (url, data, options) => {
+  return new Promise((resolve, reject) => {
+    let formData = new FormData({
+      httpsAgent: agent,
+    });
+    if (data) {
+      for (const key in data) {
+        const value = data[key];
+        if (typeof value === 'object' && Buffer.isBuffer(value)) {
+          formData.append(key, value, { knownLength: value.length, filename: key });
+          continue;
+        }
+        formData.append(key, value);
+      }
+    }
+
+    const params = parseUrl(url);
+    params.headers = {
+      'Content-Type': `multipart/form-data; boundary=${formData.getBoundary()}`,
+    };
+    params.agent = agent;
+    formData.submit(params, function (err, response) {
+      if (err) {
+        throw err;
+      }
+
+      response.resume();
+
+      let rawData = '';
+      response.on('data', (chunk) => {
+        rawData += chunk;
+      });
+      response.on('end', () => {
+        try {
+          const parsedData = JSON.parse(rawData);
+          resolve(parsedData);
+        } catch (e) {
+          reject(e);
+        }
+      });
+    });
+  });
+};
 
 const baseUrl = 'https://avmoo.cfd';
 
@@ -33,7 +83,7 @@ class Avmoo {
   static async collect_page(browser, page, page_no) {
     const last_date = '2023-08-19';
 
-    let released_url = `${baseUrl}/cn/released`;
+    let released_url = `${baseUrl}/ja/released`;
     if (page_no !== 1) {
       released_url += `/page/${page_no}`;
     }
@@ -74,7 +124,7 @@ class Avmoo {
         }
 
         const detail_page = await createPage(browser);
-        const item_detail = await Avmoo.collect_item(detail_page, { url: item_href, referer: released_url });
+        const item_detail = await Avmoo.collect_item(detail_page, { bango, url: item_href, referer: released_url });
         if (detail_page) {
           await detail_page.close();
           detail_page = null;
@@ -86,16 +136,52 @@ class Avmoo {
     return true;
   }
 
-  static async collect_item(page, { url, referer }) {
+  static async collect_item(page, { bango, url, referer }) {
     if (!url) {
       return null;
     }
 
+    const sample_images = [];
+    await page.on('response', async (response) => {
+      const url = response.url();
+      if (url.includes('/digital/video') && url.includes('-')) {
+        const url_segments = url.split('/');
+        const image_name = url_segments[url_segments.length - 1];
+        const image_buf = await response.buffer();
+        sample_images.push({ image_name, image_buf });
+      }
+    });
+
     try {
       await page.goto(url, {
+        waitUntil: 'networkidle2',
         timeout: 60000,
         referer,
       });
+
+      for (const sample_image of sample_images) {
+        const { image_name, image_buf } = sample_image;
+
+        try {
+          const form = new FormData();
+          form.append('file', image_buf, { knownLength: image_buf.length, filename: 'file' });
+          const img_path = `/films/img/${bango}/${image_name}`;
+
+          const { code, data, msg } = await uploadBuffer(
+            `https://localhost:7129/common/upload-file?imgPath=${img_path}`,
+            {
+              file: image_buf,
+            }
+          );
+          if (!code) {
+            throw new Error(msg);
+          }
+          const { upload_path } = data;
+          // TODO:
+        } catch (error) {
+          console.error(error);
+        }
+      }
     } catch (error) {}
   }
 
