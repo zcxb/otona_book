e6b548fae42501fcf7b3989c97161bd6bf6663b6
zcxb <chen13157379235@outlook.com>
2023-08-28 23:02:10 +0800

    add upload action

---
+++ otona_book_api/OtonaBookApi/Areas/Common/CommonController.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OtonaBookApi.Controllers;
+using FluentFTP;
+using OtonaBookApi.Common;
+
+namespace OtonaBookApi.Areas.Common
+{
+    [Route("common")]
+    public class CommonController : AreaControllerBase
+    {
+        [HttpPost("upload-file")]
+        public ResponseResult<object> UploadFile(
+            [FromForm] IFormFile? file,
+            [FromQuery] string imgPath)
+        {
+            if (file == null)
+            {
+                return Ok(null);
+            }
+
+            using var ftpClient = new FtpClient("ftp://localhost", "zcxb", "123456");
+            try
+            {
+                ftpClient.Connect();
+                var uploadPath = Path.Combine("otona_book", imgPath);
+                using var fStream = file.OpenReadStream();
+                ftpClient.UploadStream(fStream, uploadPath, FtpRemoteExists.Overwrite, true);
+                return new { upload_path = uploadPath };
+            }
+            finally
+            {
+                ftpClient.Disconnect();
+            }
+        }
+    }
+}
