e7907fef7bc5030bce0b48bc7e5247005e7519da
zcxb <chen13157379235@outlook.com>
2023-08-29 17:24:59 +0800

    use minio, disable https

--- otona_book_api/OtonaBookApi/Areas/Common/CommonController.cs
+++ otona_book_api/OtonaBookApi/Areas/Common/CommonController.cs
@@ -5,16 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OtonaBookApi.Controllers;
-using FluentFTP;
 using OtonaBookApi.Common;
+using Minio;
+using System.Net.Mime;
+using System.Security.AccessControl;
+
 
 namespace OtonaBookApi.Areas.Common
 {
     [Route("common")]
     public class CommonController : AreaControllerBase
     {
+        static string endpoint = "localhost:9000";
+        static string accessKey = "TmbB3PnPWseV5TwMlEe2";
+        static string secretKey = "QeUdR3hUmbu1TxFYlVdek4mCHZHovRDX9n77KYES";
+        static bool secure = false;
+
+        private static MinioClient minio = new MinioClient()
+                                            .WithEndpoint(endpoint)
+                                            .WithCredentials(accessKey, secretKey)
+                                            .WithSSL(secure)
+                                            .Build();
+
         [HttpPost("upload-file")]
-        public ResponseResult<object> UploadFile(
+        public async Task<ResponseResult<object>> UploadFile(
             [FromForm] IFormFile? file,
             [FromQuery] string imgPath)
         {
@@ -23,19 +37,15 @@ namespace OtonaBookApi.Areas.Common
                 return Ok(null);
             }
 
-            using var ftpClient = new FtpClient("ftp://localhost", "zcxb", "123456");
-            try
-            {
-                ftpClient.Connect();
-                var uploadPath = Path.Combine("otona_book", imgPath);
-                using var fStream = file.OpenReadStream();
-                ftpClient.UploadStream(fStream, uploadPath, FtpRemoteExists.Overwrite, true);
-                return new { upload_path = uploadPath };
-            }
-            finally
-            {
-                ftpClient.Disconnect();
-            }
+            var fStream = file.OpenReadStream();
+            var objectName = Path.GetFileName(imgPath);
+            var putObjectArgs = new PutObjectArgs()
+                .WithBucket("otona-book")
+                .WithObject(imgPath)
+                .WithObjectSize(fStream.Length)
+                .WithStreamData(fStream);
+            var response = await minio.PutObjectAsync(putObjectArgs);
+            return new { file_path = imgPath };
         }
     }
 }
